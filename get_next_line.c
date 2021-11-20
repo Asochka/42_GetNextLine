50abd9326cb0a44acbca9d70f2a5e8f79eef1977
Asochka <fadeevafadeeva03@gmail.com>
2021-11-20 17:40:08 +0300

    youtube_prog_all

--- get_next_line.c
+++ get_next_line.c
@@ -1,9 +1,8 @@
 #include "get_next_line.h"
-#define BUFFER_SIZE 12
 
 char	*get_next_line(int fd)
 {
-	static char	buf[BUFFER_SIZE + 1];
+	static char	buf[BUFFER_SIZE * (2 * (BUFFER_SIZE > 0) - 1) + 1];
 	static int	i;
 	static int	count_lines;
 	char		*word;
@@ -12,6 +11,8 @@ char	*get_next_line(int fd)
 	int			j;
 	long		count;
 
+	if (read(fd, (void *)0, 0) != 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	word = malloc(1);
 	if (!word)
 		return (NULL);
@@ -23,21 +24,25 @@ char	*get_next_line(int fd)
 	{
 		if (buf[i] == '\0')
 		{
-			count = read(fd, buf, BUFFER_SIZE);
+			count = read(fd, buf, BUFFER_SIZE);//number of readed bites
 			if (count <= 0)
 			{
-				printf("%s", word);
+				if (word[0])
+					return (word);
 				free(word);
-				return ("\n");
+				return (NULL);
 			}
 			buf[count] = '\0';
 			i = 0;
 		}
+		j = 0;
 		while (buf[i + j] && buf[i + j] != '\n')
 			j++;
 		word2 = malloc(j + 1 + (buf[i + j] == '\n'));
 		if (!word2)
 		{
+			if (word[0])
+				return (word);
 			free(word);
 			return (NULL);
 		}
@@ -55,20 +60,33 @@ char	*get_next_line(int fd)
 		free(word2);
 		word = word3;
 	}
-	//free(word);
-	return (word3);
+	if (j == 0 && buf[i] == '\n')
+	{
+		free(word);
+		word = malloc(2);
+		word[0] = '\n';
+		word[1] = '\0';
+	}
+	if (word[0])
+		return (word);
+	free(word);
+	return (NULL);
 }
 
-int main()
-{
-	int fd;
+// int main()
+// {
+// 	int fd;
 
-	fd = open("test.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	close(fd);
-}
+// 	fd = open("test.txt", O_RDONLY);
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	close(fd);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -3,8 +3,12 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>//todelete
-# include <stdio.h>//todelete
+// # include <fcntl.h>//todelete
+// # include <stdio.h>//todelete
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
