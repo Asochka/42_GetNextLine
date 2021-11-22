aa2f4e82c129c57acb51897332c7df722ed88253
Asochka <fadeevafadeeva03@gmail.com>
2021-11-22 13:23:20 +0300

    +bonuse

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smana <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/22 13:22:23 by smana             #+#    #+#             */
-/*   Updated: 2021/11/22 13:22:25 by smana            ###   ########.fr       */
+/*   Created: 2021/11/22 13:21:43 by smana             #+#    #+#             */
+/*   Updated: 2021/11/22 13:21:46 by smana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(int fd)
 {
-	static char	buf[BUFFER_SIZE * (2 * (BUFFER_SIZE > 0) - 1) + 1];
-	static int	i;
-	char		*word;
-	int			j;
+	static t_stat	bufi[MAX_FD];
+	char			*word;
+	int				j;
 
 	if (read(fd, (void *)0, 0) != 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -25,10 +24,10 @@ char	*get_next_line(int fd)
 	if (!word)
 		return (NULL);
 	word[0] = '\0';
-	word = ft_create_word(&i, buf, fd, &j);
+	word = ft_create_word(&bufi[fd].i, bufi[fd].buf, fd, &j);
 	if (!word)
 		return (NULL);
-	if (j == 0 && buf[i] == '\n')
+	if (j == 0 && bufi[fd].buf[bufi[fd].i] == '\n')
 	{
 		free(word);
 		word = malloc(2);
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smana <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/22 13:22:29 by smana             #+#    #+#             */
-/*   Updated: 2021/11/22 13:22:32 by smana            ###   ########.fr       */
+/*   Created: 2021/11/22 13:21:35 by smana             #+#    #+#             */
+/*   Updated: 2021/11/22 13:21:38 by smana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -21,6 +21,13 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
 # endif
+
+# define MAX_FD 256
+
+typedef struct s_stat {
+	char	buf[BUFFER_SIZE * (2 * (BUFFER_SIZE > 0) - 1) + 1];
+	int		i;
+}	t_stat;
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
