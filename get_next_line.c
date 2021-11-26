c37a23c964e7e364349429dd9a02c3d526643336
Asochka <fadeevafadeeva03@gmail.com>
2021-11-26 17:08:21 +0300

    fix_p1

--- get_next_line.c
+++ get_next_line.c
@@ -12,31 +12,94 @@
 
 #include "get_next_line.h"
 
+long	reading(int fd, char *buf, int *i)
+{
+	long	count;
+
+	count = read(fd, buf, BUFFER_SIZE);
+	if (count <= 0)
+		return (0);
+	buf[count] = '\0';
+	*i = 0;
+	return (count);
+}
+
+char	*ft_check(char *word)
+{
+	if (word[0] == '\0')
+	{
+		free (word);
+		return (NULL);
+	}
+	else
+		return (word);
+}
+
+char	*ft_slash_n(char *word)
+{
+	free(word);
+	word = malloc(2);
+	word[0] = '\n';
+	word[1] = '\0';
+	return (word);
+}
+
+char	*ft_first_word(char *word)
+{
+	word = malloc(1);
+	if (!word)
+		return (NULL);
+	word[0] = '\0';
+	return (word);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	buf[BUFFER_SIZE * (2 * (BUFFER_SIZE > 0) - 1) + 1];
 	static int	i;
 	char		*word;
-	int			j;
+	char		*word2;
+	long		count;
 
 	if (read(fd, (void *)0, 0) != 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	word = malloc(1);
-	if (!word)
-		return (NULL);
-	word[0] = '\0';
-	word = ft_create_word(&i, buf, fd, &j);
-	if (!word)
-		return (NULL);
-	if (j == 0 && buf[i] == '\n')
+	word = NULL;
+	word = ft_first_word(word);
+	if (buf[i] == '\n' && buf[++(i)] == '\n')
+		return (ft_slash_n(word));
+	while (buf[i] == '\0' || buf[i] != '\n')
 	{
-		free(word);
-		word = malloc(2);
-		word[0] = '\n';
-		word[1] = '\0';
+		if (buf[i] == '\0')
+		{
+			count = reading(fd, buf, &i);
+			if (count == 0)
+				return (ft_check(word));
+		}
+		word2 = ft_create_line(&i, buf, word, word2);
+		word = ft_strjoin(word, word2);
 	}
-	if (word[0])
-		return (word);
-	free(word);
-	return (NULL);
+	return (ft_check(word));
 }
+
+// int main()
+// {
+// 	int fd;
+// 	fd = open("test.txt", O_RDONLY);
+// 	char *s = get_next_line(fd);
+// 	char *m = get_next_line(fd);
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	free(s);
+// 	free(m);
+// 	close(fd);
+// } 
--- get_next_line.h
+++ get_next_line.h
@@ -15,15 +15,20 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>//todelete
+# include <stdio.h>//todelete
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 0
+#  define BUFFER_SIZE 1000
 # endif
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(char *s);
-char	*ft_create_line(int	*i, char	*buf, char	*word, int	*j);
-char	*ft_create_word(int	*i, char *buf, int fd, int *j);
+char	*ft_create_line(int	*i, char *buf, char	*word, char *word2);
+long	reading(int fd, char *buf, int *i);
+char	*ft_check(char *word);
+char	*ft_slash_n(char *word);
+char	*ft_first_word(char *word);
 
 #endif
